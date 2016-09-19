df8e27633c6cb7ac8fc4da414f70223e98a2b540
Randy Ferrer <randfer@gmail.com>
2016-09-19 19:25:55 -0400

    Fixed config errors, server not starting

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
-export const db = {
+exports.db = {
   host: 'localhost',
   port: 28015,
   db: 'expertsdb',
 };
 
-export const auth = {
+exports.auth = {
   passwordSalt: '9?v7ztsUpP_3pQcfaTuBkY4%=G*48snQ#-^zXgx*c7j-42X!mAj!CrkPx3dQ-gg-',
   sessionSecret: '5T++x"%~6teGj.{NrK^f59-/]-{"7sRutZ6y"=(gM5Jhd).cYLaneZc?kfT,LGtZ',
   jwtSecret: 'hUv%^a/J,GxWSMJRE\]n!%Aa\z3<?Kvr(T{/Dhg5\!~PH/`%H9.kMzS2K@g5W~\n',
